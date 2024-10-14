301dd217165cacd4ab481481b0d97515e7195757
“og-kara” <karina.karish.aleksandrova@mail.ru>
2024-10-14 20:04:51 +0300

    q3

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {Header} from './components/header/Header'
-import {Banner} from './components/banner/Banner'
-import {Catalog} from './components/catalog/Catalog'
+import {Route, Routes} from 'react-router-dom'
+import {HomePage} from './pages/HomePage/HomePage'
+import {CatalogPage} from './pages/CatalogPage/CatalogPage'
+
 
 function App() {
 
   return (
     <>
       <Header/>
-      <Banner/>
-      <Catalog/>
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/catalog' element={<CatalogPage/>} />
+      </Routes>
     </>
   )
 }
